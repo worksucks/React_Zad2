e997605f8ee198691a96d0d1b8cea1b45518e9c6
Tomasz Reizer <t_reizer@yahoo.com>
2019-01-19 06:31:08 +0100

    Added script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var movies = [
     id: 1,
     title: 'Harry Potter',
     desc: 'film o czarodzieju',
-    picture: 'https://static.antyweb.pl/wp-content/uploads/2018/11/23075023/krol-lew-2019-1420x670.jpg'
+    picture: 'https://www.pambazuka.org/sites/default/files/styles/flexslider_full/public/field/image/EW_Harry-Potter_Featured.jpg?itok=XphMvnG2'
   },
   {
     id: 2,
@@ -12,60 +12,65 @@ var movies = [
     picture: 'https://static.antyweb.pl/wp-content/uploads/2018/11/23075023/krol-lew-2019-1420x670.jpg'
   },
   {
-    id: 2,
-    title: 'Król Lew',
-    desc: 'Film o królu sawanny',
-    picture: 'https://static.antyweb.pl/wp-content/uploads/2018/11/23075023/krol-lew-2019-1420x670.jpg'
+    id: 3,
+    title: 'Sami Swoi',
+    desc: 'Film o dwóch zwaśnionych rodach z kresów',
+    picture: 'https://ssl-gfx.filmweb.pl/ph/11/13/1113/64278.2.jpg'
   },
 ];
 
 var MovieTitle = React.createClass({
 
   propTypes: {
-    movies: React.PropTypes.object.isRequired,
+    movie: React.PropTypes.object.isRequired,
   },
 
     render: function(){
-      return React.createElement('h2', {}, movie.title),
-    };
+      return React.createElement('h2', {}, this.props.movie.title)
+    }
 });
 
 var MovieDescription = React.createClass({
 
   propTypes: {
-    movies: React.PropTypes.object.isRequired,
+    movie: React.PropTypes.object.isRequired,
   },
 
     render: function() {
-      return React.createElement('p', {}, movie.desc),
-    };
+      return React.createElement('p', {}, this.props.movie.desc)
+    }
 });
 
 var MoviePicture = React.createClass({
   propTypes: {
-    movies: React.PropTypes.image.isRequired,
+    movie: React.PropTypes.object.isRequired,
   },
 
     render: function() {
-      return React.createElement('img', {src:movie.picture}),
-    };
+      return React.createElement('img', {src:this.props.movie.picture})
+    }
 
 })
 
 
 var Movie = React.createClass({
   propTypes: {
-    movies: React.PropTypes.object.isRequired,
+    movie: React.PropTypes.object.isRequired,
   },
 
   render: function() {
+
     return React.createElement('li', {},
-      return React.createElement('MovieTitle',{}, movietitle.MovieTitle),
-      return React.createElement('MovieDescription', {}, moviedescription.MovieDescription),
-      return React.createElement('MoviePicture',{}, image.MoviePicture)
+      React.createElement(MovieTitle, { movie: this.props.movie }),
+      React.createElement(MovieDescription, { movie: this.props.movie }),
+      React.createElement(MoviePicture, { movie: this.props.movie })
     )
-  },
+  }
 });
 
-var element = React.createElement(Movie, {key: movies});
+var abc = movies.map(function(item) {
+  return React.createElement(Movie, {key: item.id, movie: item})
+})
+
+var element = React.createElement('ul', {}, abc);
 ReactDOM.render(element, document.getElementById('app'));
